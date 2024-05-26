bb72165d8ff669e5d23e0d706981f4d00befc957
Akash Kumar <77935092+akashkumar23@users.noreply.github.com>
2024-05-27 03:33:25 +0530

    implement leftSideBar mobileNav rightSideBar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,41 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
+
 import React from 'react'
 
 const Home = () => {
+
+  const loggedIn = { firstName: 'Akash', lastName: 'Kumar', email: 'sample@gmail.com' }
+
   return (
-    <div>
-      Home
-    </div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          {/* Welcome, Akash */}
+          <HeaderBox
+            type='greeting'
+            title='Welcome'
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access & manage your account and transactions efficiently."
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{currentBalance: 125.56}, {currentBalance:150.70}]}
+      />
+    </section>
+
   )
 }
 
