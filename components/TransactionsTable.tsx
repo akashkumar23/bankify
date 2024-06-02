1cb6e890b5f1087f9f27099da279b237a60a41ae
Akash Kumar <77935092+akashkumar23@users.noreply.github.com>
2024-06-02 09:33:18 +0530

    implement Transaction-History-Page

--- components/TransactionsTable.tsx
+++ components/TransactionsTable.tsx
@@ -46,7 +46,7 @@ const TransactionsTable = ({transactions} : TransactionTableProps) => {
           const amount = formatAmount(t.amount);
           const isDebit = t.type === 'debit';
           const isCredit = t.type === 'credit';
-          console.log(t.name)
+          // console.log(t.name)
           return(
             <TableRow key={t.id} className={`${isDebit || amount[0] === '-' ? 'bg-[#FFFBFA]' : 'bg-[#F6FEF9]'} !over:bg-none !border-b-DEFAULT`}>
               <TableCell className='max-w-[250px] pl-2 pr-10'>
